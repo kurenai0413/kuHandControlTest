c7020f2aa67f13248bdd5b56641446c308d869a5
kurenai0413 <kurenai0413@hotmail.com>
2017-03-22 12:06:26 +0800

    Translation now is along to camera vector up, right and forward.

--- Assets/Scripts/kuGestureAction.cs
+++ Assets/Scripts/kuGestureAction.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.VR.WSA;
+using UnityEngine.VR.WSA.Persistence;
 
 namespace kuHoloLensUtility
 {
@@ -10,22 +12,86 @@ namespace kuHoloLensUtility
     {
         public Text DebugText;
 
-        public float RotationSensitivity    = 1.0f;
+        public float RotationSensitivity    = 0.5f;
 
-        public float TranslationSensitivity = 0.5f;
+        public float TranslationSensitivity = 0.3f;
+
+        public string ObjAnchorName;
 
         private Vector3 manipulationPreviousPosition;
+
+        WorldAnchorStore anchorStore;
 
         // Use this for initialization
         void Start()
         {
-
+            WorldAnchorStore.GetAsync(AnchorStoreReady);
+            AttachAnchor();
         }
 
-        // Update is called once per frame
+        void AnchorStoreReady(WorldAnchorStore store)
+        {
+            anchorStore = store;
+
+            string[] ids = anchorStore.GetAllIds();
+
+            for (int index = 0; index < ids.Length; index++)
+            {
+                Debug.Log(ids[index]);
+                if (ids[index] == ObjAnchorName)
+                {
+                    //text.text = ObjectAnchorStoreName + "founded.";
+
+                    WorldAnchor wa = anchorStore.Load(ids[index], gameObject);
+                    break;
+                }
+            }
+        }
+
+            // Update is called once per frame
         void Update()
         {
            
+        }
+
+        private void RemoveAnchor()
+        {
+            WorldAnchor anchor = gameObject.GetComponent<WorldAnchor>();
+            if (anchor != null)
+            {
+                DestroyImmediate(anchor);
+            }
+
+            string[] ids = anchorStore.GetAllIds();
+            for (int index = 0; index < ids.Length; index++)
+            {
+                if (ids[index] == ObjAnchorName)
+                {
+                    bool deleted = anchorStore.Delete(ids[index]);
+                    break;
+                }
+            }
+
+#if (ShowDebug)
+            DebugText.text = "Anchor removed.";
+#endif
+        }
+
+        private void AttachAnchor()
+        {
+            WorldAnchor attachingAnchor = gameObject.AddComponent<WorldAnchor>();
+            if (attachingAnchor.isLocated)
+            {
+                bool saved = anchorStore.Save(ObjAnchorName, attachingAnchor);
+            }
+            else
+            {
+                attachingAnchor.OnTrackingChanged += AttachingAnchor_OnTrackingChanged;
+            }
+
+#if (ShowDebug)
+            DebugText.text = "Anchor Attached.";
+#endif
         }
 
         private void PerformRotation(Vector3 position)
@@ -47,7 +113,9 @@ namespace kuHoloLensUtility
 
                 // 2.c: transform.Rotate along the Y axis using rotationFactor.
                 // rotation is along the object's local axis
+                RemoveAnchor();
                 transform.Rotate(new Vector3(rotationFactorY, -rotationFactorX, -rotationFactorZ), Space.World);
+                AttachAnchor();
             }
         }
 
@@ -55,27 +123,53 @@ namespace kuHoloLensUtility
         {
             manipulationPreviousPosition = position;
 #if ShowDebug
-            DebugText.text = "PerformManipulationStart triggered.";
+            //DebugText.text = "PerformManipulationStart triggered.";
 #endif
         }
 
         private void PerformManipulationUpdate(Vector3 position)
         {
 #if ShowDebug
-            DebugText.text = "PerformManipulationUpdate triggered. ("
-                           + position.x + ", " + position.y + ", " + position.z + ")";
+            //DebugText.text = "PerformManipulationUpdate triggered. ("
+            //               + position.x + ", " + position.y + ", " + position.z + ")";
 #endif
             
             /* TODO: DEVELOPER CODING EXERCISE 4.a */
 
-            Vector3 moveVector = Vector3.zero;
+            Vector3 RelativeVector = Vector3.zero;
             // 4.a: Calculate the moveVector as position - manipulationPreviousPosition.
-            moveVector = position - manipulationPreviousPosition;
+            RelativeVector = position - manipulationPreviousPosition;
             // 4.a: Update the manipulationPreviousPosition with the current position.
             manipulationPreviousPosition = position;
 
+            Vector3 MoveVector = Vector3.zero;
+
+            if (RelativeVector.x != 0)
+            {
+                MoveVector = RelativeVector.x * Camera.main.transform.right;
+            }
+            if (RelativeVector.y != 0)
+            {
+                MoveVector = RelativeVector.y * Camera.main.transform.up;
+            }
+            if (RelativeVector.z != 0)
+            {
+                MoveVector = RelativeVector.z * Camera.main.transform.forward;
+            }
+
             // 4.a: Increment this transform's position by the moveVector.
-            transform.position += TranslationSensitivity * moveVector;
+            RemoveAnchor();
+            transform.position += TranslationSensitivity * MoveVector;
+            AttachAnchor();
+        }
+
+        private void AttachingAnchor_OnTrackingChanged(WorldAnchor self, bool located)
+        {
+            if (located)
+            {
+                bool saved = anchorStore.Save(ObjAnchorName, self);
+                self.OnTrackingChanged -= AttachingAnchor_OnTrackingChanged;
+            }
         }
     }
 }
